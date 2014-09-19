eb0459cad477097816242ba3aa865d2941a536ee
Adam Caprez <acaprez2@unl.edu>
2014-09-19 17:29:18 -0500

    Add muscle recipe.

---
+++ muscle/3.8.31/build.sh
@@ -0,0 +1,3 @@
+mkdir -pv $PREFIX/bin
+cp muscle3.8.31_i86linux64 $PREFIX/bin
+#ln -s $PREFIX/bin/muscle3.8.31_i86linux64 $PREFIX/bin/muscle
---
+++ muscle/3.8.31/post-link.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -s $PREFIX/bin/muscle3.8.31_i86linux64 $PREFIX/bin/muscle

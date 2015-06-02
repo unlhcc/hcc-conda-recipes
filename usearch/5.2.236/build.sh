4a4357850e4b29491f6d20f5eabd66393b9ba481
Adam Caprez <acaprez2@unl.edu>
2015-06-02 14:09:10 -0500

    Add recipe for USEARCH 5.x

---
+++ usearch/5.2.236/build.sh
@@ -0,0 +1,3 @@
+mkdir -pv $PREFIX/bin
+chmod +x usearch5.2.236_i86linux32
+cp usearch5.2.236_i86linux32 $PREFIX/bin
---
+++ usearch/5.2.236/post-link.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -s $PREFIX/bin/usearch5.2.236_i86linux32 $PREFIX/bin/usearch

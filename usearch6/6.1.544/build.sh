bf7ff727ab7755f4dab727eb22622dfa5ae23bcd
Adam Caprez <acaprez2@unl.edu>
2015-06-02 14:09:45 -0500

    Add recipe for USEARCH 6.x
    Giving this a separate name to work with QIIME.  QIIME requires
    both 5.x and 6.x, with names `usearch` and `usearch61` respectively.
    Only way to make that work is two differently named packages.

---
+++ usearch6/6.1.544/build.sh
@@ -0,0 +1,3 @@
+mkdir -pv $PREFIX/bin
+chmod +x usearch6.1.544_i86linux32
+cp usearch6.1.544_i86linux32 $PREFIX/bin
---
+++ usearch6/6.1.544/post-link.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -s $PREFIX/bin/usearch6.1.544_i86linux32 $PREFIX/bin/usearch61

86c3f64a3206f0b6fc6285ec3d89940fcf313c98
Adam Caprez <acaprez2@unl.edu>
2015-06-25 11:29:19 -0500

    Add recipe for ScientificPython

---
+++ scientificpython/2.8.1/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export NETCDF_PREFIX=${PREFIX}
+
+$PYTHON setup.py build
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.

772bb0bb1fe970b2878e8900a3762fbcc8ee0409
Adam Caprez <acaprez2@unl.edu>
2015-06-26 15:54:24 -0500

    Add recipe for pyhdf

---
+++ pyhdf/0.8.3/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export INCLUDE_DIRS=${PREFIX}/include
+export LIBRARY_DIRS=${PREFIX}/lib
+
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.

0e3fe0fdd6e35b8e63e66b8eeab446309ef8130f
Adam Caprez <acaprez2@unl.edu>
2015-06-26 17:11:57 -0500

    Add recipe for r-nlme for R 3.1.2

---
+++ r-nlme/3.1_120/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
+# These recursively depend on r-nlme, since it is a recommended package, so
+# install them manually
+
+conda install --no-deps r-lattice
+
+$R CMD INSTALL --build .
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.

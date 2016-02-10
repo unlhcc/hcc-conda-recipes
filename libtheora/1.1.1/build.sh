9f089ca5eea79b9cec82afe76ef510186a484f23
Adam Caprez <acaprez2@unl.edu>
2016-02-10 22:55:25 +0000

    Add recipe for libtheora 1.1.1

---
+++ libtheora/1.1.1/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LDFLAGS="-L${PREFIX}/lib"
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+
+./configure --prefix=${PREFIX}
+make
+make install

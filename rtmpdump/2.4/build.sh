328887f85168a686fe5610e69389cd9a3b96940a
Adam Caprez <acaprez2@unl.edu>
2016-02-11 23:45:59 +0000

    Switch to make install, tweak install paths

--- rtmpdump/2.4/build.sh
+++ rtmpdump/2.4/build.sh
@@ -4,8 +4,4 @@ mkdir -pv ${PREFIX}/bin ${PREFIX}/share/man/man1 ${PREFIX}/share/man/man3 ${PREF
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 make SYS=posix
-cp {rtmpdump,rtmpgw,rtmpsrv,rtmpsuck} ${PREFIX}/bin
-cp librtmp/{librtmp.so.1,librtmp.so} ${PREFIX}/lib
-cp rtmpdump.1 ${PREFIX}/share/man/man1
-cp rtmpgw.8 ${PREFIX}/share/man/man8
-cp librtmp/librtmp.3 ${PREFIX}/share/man/man3
+make install

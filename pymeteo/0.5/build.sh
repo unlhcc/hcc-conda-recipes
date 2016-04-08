2353c44f4fc90d231022440e6da1f3e7bd53a92c
Adam Caprez <acaprez2@unl.edu>
2016-04-08 16:47:11 -0500

    Update PyMeteo recipe

--- pymeteo/0.5/build.sh
+++ pymeteo/0.5/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 $PYTHON setup.py install
-
+mv ${PREFIX}/bin/cm1_geninit ${PREFIX}/bin/cm1_geninit.py
+cat > ${PREFIX}/bin/cm1_geninit <<EOF 
+#!/bin/bash
+QT_API=pyqt5 QT_QPA_PLATFORM_PLUGIN_PATH=/opt/anaconda1anaconda2anaconda3/lib/qt5/plugins/platforms cm1_geninit.py
+EOF
+chmod +x ${PREFIX}/bin/cm1_geninit
 # Add more build steps here, if they are necessary.
 
 # See
---
+++ pymeteo/0.5/run_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+QT_API=pyqt5 QT_QPA_PLATFORM_PLUGIN_PATH=/home/swanson/acaprez/.conda/envs/_test/lib/qt5/plugins/platforms python test.py

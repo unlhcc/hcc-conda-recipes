939b9ff77c876f231f2788cc2db3c0a6f071ed50
Adam Caprez <acaprez2@unl.edu>
2017-03-02 00:56:23 +0000

    Add recipe for tensorflow-cpucompat 1.0.0.

---
+++ recipes/tensorflow-cpucompat/1.0.0/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export CPATH=${PREFIX}/include
+
+# Create a unique tmpdir for the .whl to avoid name collisions
+TMPDIR=`mktemp -d`
+
+# Make sure build env is clean when doing multiple Python versions
+bazel clean
+
+# set needed env vars so configure will run non-interactively
+export PYTHON_BIN_PATH=${PYTHON}
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+export CC_OPT_FLAGS="-march=corei7-avx"
+export TF_NEED_JEMALLOC=0
+export TF_ENABLE_XLA=0
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_NEED_OPENCL=0
+export TF_NEED_CUDA=0
+
+./configure
+
+# Patch protobuf to use rpath for the newer compiler libs.
+# This has to be done during the build since configure downloads
+# protobuf into the unique build directory.
+PROTOBUF_DIR=$(bazel info output_base)/external/protobuf
+patch -p0 -i ${RECIPE_DIR}/protobuf.patch -d ${PROTOBUF_DIR}
+
+bazel build --define PREFIX=${PREFIX} --local_resources 4096,4,1.0 -c opt --copt=-mfpmath=both //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package ${TMPDIR}
+pip install ${TMPDIR}/tensorflow*.whl

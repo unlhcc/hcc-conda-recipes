0d38199016752d8254a5eb8314c9513e0aadece6
Adam Caprez <acaprez2@unl.edu>
2017-03-22 15:26:39 +0000

    libwebp is in conda-forge, so remove it here.

--- recipes/libwebp/0.5.0/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
-./autogen.sh
-./configure --prefix=${PREFIX} --with-pkgconfigdir=${PREFIX}/lib/pkgconfig
-make
-make install

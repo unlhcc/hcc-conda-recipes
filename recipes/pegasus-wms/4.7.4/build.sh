a62636eea46ca6d61d2eb0da6ef0da1514fe5f0b
Adam Caprez <acaprez2@unl.edu>
2017-04-17 19:29:00 +0000

    Enable support for MySQL/PostgreSQL at build time.

--- recipes/pegasus-wms/4.7.4/build.sh
+++ recipes/pegasus-wms/4.7.4/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export C_INCLUDE_PATH=${PREFIX}/include
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
+
 ant dist
 cp -R dist/pegasus-4.7.4/{bin,etc,lib,share} ${PREFIX}

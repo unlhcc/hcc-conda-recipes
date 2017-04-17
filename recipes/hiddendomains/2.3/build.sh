bbb3120875f1e2a82da7a16eaf1b62840349377c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-04-17 02:15:52 +0000

    Modified recipe for hiddendomains to include r libraries

--- recipes/hiddendomains/2.3/build.sh
+++ recipes/hiddendomains/2.3/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir -pv $PREFIX/bin
-cp hiddenDomains peakCenters *.pl $PREFIX/bin
+sed "s/\.\/binReads.pl/binReads.pl/g" hiddenDomains
+sed "s/\.\/domainsToBed.pl/domainsToBed.pl/g" hiddenDomains
+sed "s/\.\/domainsMergeToBed.pl/domainsMergeToBed.pl/g" hiddenDomains
+cp hiddenDomains peakCenters *.pl *.R $PREFIX/bin

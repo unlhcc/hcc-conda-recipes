322f549db62c1a1faac708f5d0ef6fee66be669c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-04-18 18:23:46 -0500

    Delete build.sh

--- recipes/hiddendomains/2.3/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -pv $PREFIX/bin
-sed "s/\.\/binReads.pl/binReads.pl/g" hiddenDomains
-sed "s/\.\/domainsToBed.pl/domainsToBed.pl/g" hiddenDomains
-sed "s/\.\/domainsMergeToBed.pl/domainsMergeToBed.pl/g" hiddenDomains
-cp hiddenDomains peakCenters *.pl *.R $PREFIX/bin

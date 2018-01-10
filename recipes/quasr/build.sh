f66a2b1afd1a2f79cf272c50bd8f53c55b654f57
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-01-10 17:27:32 -0600

    Update recipe

--- recipes/quasr/build.sh
+++ recipes/quasr/build.sh
@@ -2,4 +2,8 @@
 
 cp *.py ${PREFIX}/bin
 cp extras/* ${PREFIX}/bin
-cp -R modules/ ${PREFIX}
+cp -R modules/ ${SP_DIR}/quasr
+touch ${SP_DIR}/quasr/__init__.py
+echo ${SP_DIR}
+ls ${SP_DIR}
+ls ${SP_DIR}/quasr

0012f3146ac2db5fa63fc1af08267adf4473299f
Adam Caprez <acaprez2@unl.edu>
2018-01-16 20:01:49 +0000

    Add config file and CABAL_CONFIG variable.

--- recipes/cabal-install/2.0.0.0/build.sh
+++ recipes/cabal-install/2.0.0.0/build.sh
@@ -3,3 +3,10 @@
 export LD_LIBRARY_PATH=${PREFIX}/lib
 
 EXTRA_CONFIGURE_OPTS="--extra-include-dirs=${PREFIX}/include --extra-lib-dirs=${PREFIX}/lib" ./bootstrap.sh  --global
+
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+
+echo "export CABAL_CONFIG=${PREFIX}/etc/cabal.config" > ${PREFIX}/etc/conda/activate.d/cabal-config.sh
+echo "unset CABAL_CONFIG" > ${PREFIX}/etc/conda/deactivate.d/cabal-config.sh
+
+cp ${RECIPE_DIR}/cabal.config ${PREFIX}/etc

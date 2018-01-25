b2d21513070180f09c63b083795c7072bf249607
Adam Caprez <acaprez2@unl.edu>
2018-01-25 21:31:34 +0000

    Remove duplicate files.

--- recipes/ksnp/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/bin
-cp $SRC_DIR/kSNP3.1_Linux_package/kSNP3/* $PREFIX/bin/
--- recipes/pymagic/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-cp ./src/magic/MAGIC.py $PREFIX/bin

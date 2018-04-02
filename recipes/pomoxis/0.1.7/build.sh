2d016f2e438315fefa2f4de1703bdf8d09942705
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-04-02 16:46:03 -0500

    Fix sed to work for osx

--- recipes/pomoxis/0.1.7/build.sh
+++ recipes/pomoxis/0.1.7/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # fix for pyscrap
-find $SRC_DIR -type f -exec sed -i 's/from pomoxis\.pyscrap import pyscrap/import pyscrap/g' {} +
+find $SRC_DIR -type f -exec sed -i.bak 's/from pomoxis\.pyscrap import pyscrap/import pyscrap/g' {} +
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt

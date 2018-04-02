a4264a46866219ca11f3eccccfd59dab0a1cfb60
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-04-02 16:08:54 -0500

    Add reipce for pyscrap

--- recipes/pyscrap/0.0.9/build.sh
+++ recipes/pyscrap/0.0.9/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export LC_CTYPE=en_US.UTF-8
+if [[ "${PY_VER}" =~ 3 ]]
+then
+    2to3 -w -n setup.py
+    2to3 -w -n bin/*
+    2to3 -w -n pyscrap
+fi
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt

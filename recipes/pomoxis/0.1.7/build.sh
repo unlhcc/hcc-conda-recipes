075154df93f450ad0058c712c9c9a80151100b7f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-04-02 16:16:47 -0500

    Add recipe for pomoxis

---
+++ recipes/pomoxis/0.1.7/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# fix for pyscrap
+find $SRC_DIR -type f -exec sed -i 's/from pomoxis\.pyscrap import pyscrap/import pyscrap/g' {} +
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt

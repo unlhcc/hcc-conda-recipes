7cf4af6c9d28eb9292470834a3054b48c3953904
Adam Caprez <acaprez2@unl.edu>
2018-04-16 20:39:28 +0000

    Add recipe for cuda-driver 390.46.

--- recipes/cuda-driver/390.46/build.sh
+++ recipes/cuda-driver/390.46/build.sh
@@ -4,13 +4,13 @@
 # of time and create an archive that can be extracted to create
 # the conda package.  Not great, but it works.  To create the
 # archive, run
-# export PREFIX=$PWD/cuda-375.66
-#./NVIDIA-Linux-x86_64-375.66.run -s --no-kernel-module \
+# export PREFIX=$PWD/cuda-390.46
+#./NVIDIA-Linux-x86_64-390.46.run -s --no-kernel-module \
 #  --x-prefix=${PREFIX} --x-module-path=${PREFIX}/lib/xorg/modules \
 #  --x-library-path=${PREFIX}/lib --x-sysconfig-path=${PREFIX}/etc/X11 \
 #  --opengl-prefix=${PREFIX} --utility-prefix=${PREFIX} \
 #  --documentation-prefix=${PREFIX} --compat32-libdir=${PREFIX}/lib --no-install-libglvnd
-# tar -czvf nvidia-driver-375.66.tgz cuda-375.66
+# tar -czvf nvidia-driver-390.46.tgz cuda-390.46
 
 # Upload the archive to Box with a public shared link and update meta.yaml.
 

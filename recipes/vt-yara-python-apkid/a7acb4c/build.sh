bad397d6bf9af01175066881586df245fcf1b1df
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-04-20 16:08:49 -0500

    Add recipes for vt-yara-python and vt-yara-python-apkid

---
+++ recipes/vt-yara-python-apkid/a7acb4c/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x -e
+
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+$PYTHON setup.py install

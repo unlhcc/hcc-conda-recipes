94cf8ad9227822868edfac24292add49bb72b0d9
Adam Caprez <acaprez2@unl.edu>
2018-04-24 18:29:33 +0000

    Add workaround for UTF chars in filenames causing conda-build errors.

--- recipes/aspera-cli/3.7.7/build.sh
+++ recipes/aspera-cli/3.7.7/build.sh
@@ -2,4 +2,8 @@
 
 bash aspera-cli-3.7.7.608.927cce8-linux-64-release.sh
 
+# Workaround for Unicode chars in filenames causing conda-build to error out
+mv ~/.aspera/cli/certs/Certinomis* ~/.aspera/cli/certs/'Certinomis - Autorite Racine.crt'
+mv ~/.aspera/cli/certs/NetLock\ Arany* ~/.aspera/cli/certs/'NetLock Arany (Class Gold) Fotanusitvany.crt'
+
 cp -R ~/.aspera/cli/* ${PREFIX}

f4b8a23c3d2c95278e30df6eb1613062cfbcf954
Adam Caprez <acaprez2@unl.edu>
2018-05-29 20:30:50 +0000

    Add gnuplot 5.2.3 w/x11 support.
    The conda-forge package is built w/o x11 support.

---
+++ recipes/gnuplot/5.2.3/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export CXXFLAGS="$CXXFLAGS -std=c++11"
+
+if [ "$(uname)" == "Linux" ]
+then
+    export LDFLAGS="$LDFLAGS -L $PREFIX/lib -liconv"
+fi
+
+./configure \
+    --prefix=$PREFIX \
+    --without-lua \
+    --without-latex \
+    --without-libcerf \
+    --with-qt=qt5 \
+    --with-readline=$PREFIX \
+    --without-tutorial \
+    --without-linux-vga \
+    --enable-history-file \
+    --disable-wxwidgets
+
+export GNUTERM=dumb
+make PREFIX=$PREFIX
+make check PREFIX=$PREFIX
+make install PREFIX=$PREFIX

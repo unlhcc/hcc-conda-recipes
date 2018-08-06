c6db6412e50af3c26edfb8bb5e7246a6e31add63
Adam Caprez <acaprez2@unl.edu>
2018-08-06 23:36:22 +0000

    Cb3 migration

---
+++ recipes/mro/3.5.0/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for f in `ls -1 rpm`
+do
+    rpm2cpio rpm/$f | cpio -idm
+done
+
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib
+
+cp -aR opt/microsoft/ropen/${PKG_VERSION}/{lib64,share} ${PREFIX}
+mv ${PREFIX}/lib64 ${PREFIX}/lib
+cp opt/microsoft/ropen/${PKG_VERSION}/stage/Linux/bin/x64/*.so ${PREFIX}/lib/lib64/R/lib
+
+ln -s ${PREFIX}/lib/lib64/R/bin/R ${PREFIX}/bin/R
+ln -s ${PREFIX}/lib/lib64/R/bin/Rscript ${PREFIX}/bin/Rscript

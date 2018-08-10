ae875b92715593aea4b560f2edc6e6a4c9b7435b
Adam Caprez <acaprez2@unl.edu>
2018-08-10 15:48:16 +0000

    Add recipe for AFNI 18.2.06.

---
+++ recipes/afni/18.2.06/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include:$C_INCLUDE_PATH
+export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=${PREFIX}/lib:$LIBRARY_PATH
+export CC=${GCC}
+
+yum install -y tcsh
+
+cd src
+cp Makefile.linux_centos_7_64 Makefile
+make programs
+make install

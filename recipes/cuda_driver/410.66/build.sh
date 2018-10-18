93bc20422f31fff9a304f05df0085ae38e4830ce
Adam Caprez <acaprez2@unl.edu>
2018-10-18 22:56:06 +0000

    Add recipe for cuda_driver 410.66.

---
+++ recipes/cuda_driver/410.66/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The driver install must be run as root, so we do that ahead
+# of time and create an archive that can be extracted to create
+# the conda package.  Not great, but it works.
+# Download from https://www.nvidia.com/object/unix.html
+# To create the archive, run
+# export PREFIX=$PWD/cuda-410.66
+#./NVIDIA-Linux-x86_64-410.66.run -s --no-kernel-module \
+#  --x-prefix=${PREFIX} --x-module-path=${PREFIX}/lib/xorg/modules \
+#  --x-library-path=${PREFIX}/lib --x-sysconfig-path=${PREFIX}/etc/X11 \
+#  --opengl-prefix=${PREFIX} --utility-prefix=${PREFIX} \
+#  --documentation-prefix=${PREFIX} --compat32-libdir=${PREFIX}/lib --no-install-libglvnd
+# tar -czvf nvidia-driver-410.66.tgz cuda-410.66
+
+# Upload the archive to Box with a public shared link and update meta.yaml.
+
+cp -R {bin,lib,share} ${PREFIX}

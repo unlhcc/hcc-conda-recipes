d59f48a43d78d639a4392ede4df63009a80447b0
Adam Caprez <acaprez2@unl.edu>
2018-10-19 15:21:30 -0500

    Remove racon; exists in bioconda.

--- recipes/racon/1.2.0/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-mno-avx" -Dracon_build_wrapper=ON ..
-make -j 2
-make install
-
-cp bin/racon_wrapper ${PREFIX}/bin

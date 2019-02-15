e70d64b4ff96db561381a2b7c87f5c83bf4eef23
Daniel Nicorici <Daniel.Nicorici@gmail.com>
2019-02-15 13:03:56 +0200

    clean the old files before downloading
    Without this fix the re-downloading the database will not work.

--- recipes/fusioncatcher/1.00/download-db.sh
+++ recipes/fusioncatcher/1.00/download-db.sh
@@ -12,6 +12,7 @@ echo "Downloading Human Ensembl v90 database to ${FC_DB_PATH}/current/..."
 
 # Direct download:
 cd ${FC_DB_PATH}
+rm -rf human_v90.tar.gz.*
 wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v90.tar.gz.aa
 wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v90.tar.gz.ab
 wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v90.tar.gz.ac

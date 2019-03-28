38eb38e33ec3c8f2668cd1277b70d6e8a78ccc0f
Adam Caprez <acaprez2@unl.edu>
2019-03-28 20:42:06 +0000

    Update mro 3.4.4 install paths and pinnings.

--- recipes/mro/3.4.4/build.sh
+++ recipes/mro/3.4.4/build.sh
@@ -5,11 +5,13 @@ do
     rpm2cpio rpm/$f | cpio -idm
 done
 
-mkdir -p ${PREFIX}/bin ${PREFIX}/lib
+patch -p0 < ${RECIPE_DIR}/pkgconfig.patch
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib ${PREFIX}/lib/pkgconfig
 
-cp -aR opt/microsoft/ropen/3.4.4/{lib64,share} ${PREFIX}
-mv ${PREFIX}/lib64 ${PREFIX}/lib
-cp opt/microsoft/ropen/3.4.4/stage/Linux/bin/x64/*.so ${PREFIX}/lib/lib64/R/lib
+cp -aR opt/microsoft/ropen/${PKG_VERSION}/share ${PREFIX}
+cp -aR opt/microsoft/ropen/${PKG_VERSION}/lib64/pkgconfig/* ${PREFIX}/lib/pkgconfig
+cp -aR  opt/microsoft/ropen/${PKG_VERSION}/lib64/R ${PREFIX}/lib
+cp opt/microsoft/ropen/${PKG_VERSION}/stage/Linux/bin/x64/*.so ${PREFIX}/lib/R/lib
 
-ln -s ${PREFIX}/lib/lib64/R/bin/R ${PREFIX}/bin/R
-ln -s ${PREFIX}/lib/lib64/R/bin/Rscript ${PREFIX}/bin/Rscript
+ln -s ${PREFIX}/lib/R/bin/R ${PREFIX}/bin/R
+ln -s ${PREFIX}/lib/R/bin/Rscript ${PREFIX}/bin/Rscript

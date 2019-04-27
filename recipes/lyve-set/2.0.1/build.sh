2f5783a281333db52896a83d2f36ba41237bedfd
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-27 01:18:08 +0000

    Update recipe for lyve set

--- recipes/lyve-set/2.0.1/build.sh
+++ recipes/lyve-set/2.0.1/build.sh
@@ -5,8 +5,12 @@ cp scripts/*.pl ${PREFIX}/bin
 cp scripts/*.sh ${PREFIX}/bin
 cp scripts/cpanm ${PREFIX}/bin
 cp scripts/getgbk ${PREFIX}/bin
-cp ${PREFIX}/bin/snap-aligner ${PREFIX}/bin/snap
+ln -s ${PREFIX}/bin/snap-aligner ${PREFIX}/bin/snap
 cp plugins/tinsel/set_plugin_tinsel.pl ${PREFIX}/bin
+
+# remove varscan.sh and create symlink
+rm ${PREFIX}/bin/varscan.sh
+ln -s ${PREFIX}/bin/varscan ${PREFIX}/bin/varscan.sh
 
 # create folder for config
 # target=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}

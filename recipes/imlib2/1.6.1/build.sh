93b68b3c09f130e49363f3805cc8adfdf841cd32
Adam Caprez <acaprez2@unl.edu>
2020-01-04 00:13:47 +0000

    Add X11 support to imlib2.

--- recipes/imlib2/1.6.1/build.sh
+++ recipes/imlib2/1.6.1/build.sh
@@ -5,9 +5,9 @@ export LD_LIBRARY_PATH=${PREFIX}/lib
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 
 if [ `uname` == Darwin ]; then
-    ./configure --prefix=${PREFIX} --without-x --disable-amd64 --disable-mmx
+    ./configure --prefix=${PREFIX} --with-x --disable-amd64 --disable-mmx
 else
-    ./configure --prefix=${PREFIX} --without-x --enable-amd64 --disable-mmx
+    ./configure --prefix=${PREFIX} --with-x --enable-amd64 --disable-mmx
 fi
 
 make

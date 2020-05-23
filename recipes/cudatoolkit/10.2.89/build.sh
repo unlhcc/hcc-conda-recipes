aad119141d23551f294b1e1ec7878e0feb6c6a30
Adam Caprez <acaprez2@unl.edu>
2020-05-23 01:49:41 +0000

    Fix issue with missing cuBLAS libs.
    The main installer doesn't seem to install the cuBLAS
    libs when using a custom path. Instead use the toolkit-only
    installer (extracted from main package).

--- recipes/cudatoolkit/10.2.89/build.sh
+++ recipes/cudatoolkit/10.2.89/build.sh
@@ -3,10 +3,12 @@
 # System perl path is hardcoded in a script two extraction levels down. Grr.
 sudo yum -y install perl
 
-FN='la9zf9s9rygp5nyvj5enlpfyr1vpmqyr.run'
-#FN='cuda_10.2.89_440.33.01_rhel6.run'
+FN='qp8hkfd2kk57d8320ufmaj5u1g358is0.run'
+#FN='cuda-linux.10.2.89-27506705.run'
 chmod +x $FN
-./$FN --toolkit --silent --no-opengl-libs --override --toolkitpath=${PREFIX}
+./$FN -noprompt -nosymlink -no-man-page -prefix=${PREFIX}
 rm -rf ${PREFIX}/samples
 mv ${PREFIX}/lib64 ${PREFIX}/lib
-sed -i s/lib64/lib/g ${PREFIX}/pkgconfig/*.pc
+
+# pkgconfig dir seems to no longer be there
+#sed -i s/lib64/lib/g ${PREFIX}/pkgconfig/*.pc

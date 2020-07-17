4856751433b51ae2b74b1cc7a8f3f4fc810aff38
Adam Caprez <acaprez2@unl.edu>
2020-07-17 22:11:31 +0000

    Add cutruntools 2020.07.10.

---
+++ recipes/cutruntools/2020.07.10/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/bin
+
+$CC $CFLAGS $LDFLAGS -O2 kseq_test.c -lz -o kseq_test
+cp kseq_test ${PREFIX}/bin
+
+outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+mkdir -p $outdir
+cp -R . $outdir
+
+for f in `ls -1 $outdir/*.py $outdir/*.sh $outdir/*.R`
+do
+  ln -s $f ${PREFIX}/bin
+done
+
+chmod +x ${PREFIX}/bin/*.py ${PREFIX}/bin/*.sh
+envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}:${SP}' < config.json > $outdir/config.json

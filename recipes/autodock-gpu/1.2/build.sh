57fb372ab37d68e8000c6e14fab540132f384c5a
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2020-12-16 20:58:14 +0000

    Add recipe for autodock 1.2

---
+++ recipes/autodock-gpu/1.2/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/bin
+
+export GPU_INCLUDE_PATH=${PREFIX}/include
+export GPU_LIBRARY_PATH=${PREFIX}/lib
+
+make DEVICE=GPU NUMWI=8 CPP=${CXX}
+make DEVICE=GPU NUMWI=16 CPP=${CXX}
+make DEVICE=GPU NUMWI=32 CPP=${CXX}
+make DEVICE=GPU NUMWI=64 CPP=${CXX}
+make DEVICE=GPU NUMWI=128 CPP=${CXX}
+make DEVICE=GPU NUMWI=256 CPP=${CXX}
+
+cp bin/* ${PREFIX}/bin

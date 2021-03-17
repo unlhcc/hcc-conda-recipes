45cced5de62b5c5500a26932b264e42fc593fbdf
Adam Caprez <acaprez2@unl.edu>
2021-03-17 13:13:39 +0000

    Move debugger/cupti/cublas files into standard location.

--- recipes/cudatoolkit/10.2.89/build.sh
+++ recipes/cudatoolkit/10.2.89/build.sh
@@ -9,9 +9,16 @@ chmod +x $FN
 ./$FN -noprompt -nosymlink -no-man-page -prefix=${PREFIX}
 rm -rf ${PREFIX}/samples
 mv ${PREFIX}/lib64 ${PREFIX}/lib
+mv ${PREFIX}/extras/Debugger/lib64/* ${PREFIX}/lib
+mv ${PREFIX}/extras/Debugger/include/* ${PREFIX}/include
+mv ${PREFIX}/extras/CUPTI/lib64/* ${PREFIX}/lib
+mv ${PREFIX}/extras/CUPTI/include/* ${PREFIX}/include
+mv ${PREFIX}/cuBLAS/lib64/stubs ${PREFIX}/lib/stubs
+mv ${PREFIX}/cuBLAS/lib64/* ${PREFIX}/lib
+mv ${PREFIX}/cuBLAS/include/* ${PREFIX}/include
 
 # remove unneeded bits to get the size more manageable
-rm -rf ${PREFIX}/{doc,extras,libnsight,nsight-compute-2019.5.0,nsightee_plugins,nsight-systems-2019.5.2}
+rm -rf ${PREFIX}/{doc,extras,libnsight,nsight-compute-2019.5.0,nsightee_plugins,nsight-systems-2019.5.2,cuBLAS}
 
 # pkgconfig dir seems to no longer be there
 #sed -i s/lib64/lib/g ${PREFIX}/pkgconfig/*.pc

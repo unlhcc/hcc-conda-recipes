8e3be880bc0748437fbcc059b8a7336aa580b85a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-02-03 10:31:39 -0600

    Add adapters to scythe recipe

--- recipes/scythe/0.991/build.sh
+++ recipes/scythe/0.991/build.sh
@@ -2,5 +2,20 @@
 
 mkdir -p ${PREFIX}/bin
 
+
 make CC=${CC}
 cp scythe ${PREFIX}/bin
+
+# Create directory for adapters and copy data
+ADAPTERS="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/adapters"
+mkdir -p "${ADAPTERS}"
+cp illumina_adapters.fa ${ADAPTERS}/
+
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/scythe.sh
+export SCYTHE_ADAPTERS=${ADAPTERS}
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/scythe.sh
+unset SCYTHE_ADAPTERS
+EOF

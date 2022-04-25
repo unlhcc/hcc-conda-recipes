2e98cba676a7ae81d4627ce26f793185bab10336
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-04-24 23:35:32 -0500

    Add fortran to libint-cp2k

--- recipes/libint-cp2k/2.6.0/build.sh
+++ recipes/libint-cp2k/2.6.0/build.sh
@@ -3,6 +3,6 @@
 set -x
 export CXXFLAGS="${CXXFLAGS} -fopenmp"
 
-./configure --prefix=$PREFIX --with-boost=$PREFIX
-make -j4
-make install -j4
+./configure --prefix=$PREFIX --with-boost=$PREFIX --enable-fortran
+make -j8
+make install -j8

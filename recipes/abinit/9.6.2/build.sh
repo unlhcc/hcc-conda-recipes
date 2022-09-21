f2978401f3069d7cd0bf91a26e37c2e0dd9b7f87
Adam Caprez <acaprez2@unl.edu>
2022-09-21 18:09:04 -0500

    Add recipe for abinit 9.6.2.

---
+++ recipes/abinit/9.6.2/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+./config/scripts/makemake
+./configure --prefix=${PREFIX} \
+            --with-mpi="yes" --enable-mpi-io="yes" \
+            --with-libxc="${PREFIX}" \
+            --with-hdf5="${PREFIX}" \
+            --with-fftw3="${PREFIX}" \
+            --with-netcdf="${PREFIX}" \
+            --with-netcdf-fortran="${PREFIX}" \
+            --with-fft-flavor="fftw3" \
+            --with-libxml2="${PREFIX}" \
+            --with-xmlf90="${PREFIX}" \
+            --with-libpsml="${PREFIX}" \
+            --enable-openmp=yes \
+            --with-linalg-flavor="netlib" \
+            --with-gpu=no \
+            CC="mpicc" \
+            FC="mpifort" \
+            CXX="mpicxx" \
+            CFLAGS="${CFLAGS}" \
+            FFLAGS="${FFLAGS}" \
+            FCFLAGS="${FCFLAGS}" \
+            CPPFLAGS="${CPPFLAGS}" \
+            LDFLAGS="${LDFLAGS} -lfftw3f" \
+            WANNIER90_LIBS="-lwannier" \
+            WANNIER90_CPPFLAGS="-I${PREFIX}" \
+            WANNIER90_FCFLAGS="-I${PREFIX}"  \
+            WANNIER90_LDFLAGS="-L${PREFIX}"
+
+make -j 4
+make check
+make install

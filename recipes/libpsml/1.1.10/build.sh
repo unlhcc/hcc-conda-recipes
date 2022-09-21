1d41b0a1796ee52996a79f6fe1c39327ee89e9db
Adam Caprez <acaprez2@unl.edu>
2022-09-21 14:29:08 -0500

    Build static lib instead, as shared is broken.

--- recipes/libpsml/1.1.10/build.sh
+++ recipes/libpsml/1.1.10/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no --with-xmlf90=${PREFIX}
+# Build the static lib version, shared lib is broken: https://gitlab.com/siesta-project/libraries/libpsml/-/issues/4
+./configure --prefix=${PREFIX} --enable-shared=no --enable-static=yes --with-xmlf90=${PREFIX}
 make
 make install

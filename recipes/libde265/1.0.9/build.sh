dea7185e64bbee6ea2fda2db704e3d74964b7a0d
Adam Caprez <acaprez2@unl.edu>
2023-01-21 01:06:56 -0600

    De265 pkgconfig

--- recipes/libde265/1.0.9/build.sh
+++ recipes/libde265/1.0.9/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir build && pushd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-..
-
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make -j 4
 make install
+popd
+
+# cmake install doesn't include pkgconfig data
+./configure --prefix=${PREFIX} --disable-static --disable-sherlock265
+make install-pkgconfigDATA

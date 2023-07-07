c3596965f68b0599a9b0973e32a1e18166a80553
Adam Caprez <acaprez2@unl.edu>
2023-07-07 00:39:56 -0500

    Update afni recipe.

--- recipes/afni/23.1.08/build.sh
+++ recipes/afni/23.1.08/build.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-mkdir build
+# hush git
+export EMAIL="hcc-support@unl.edu"
+export GIT_AUTHOR_NAME="HCC CI"
+export GIT_COMMITTER_NAME="HCC CI"
+
+# workaround to set version for non-git build
+export GIT_DESCRIBE_TAG="${PKG_VERSION}"
+
+mkdir -p build/afni_data
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCOMP_GUI=ON -DCOMP_PLUGINS=ON -DSTANDARD_PYTHON_INSTALL=ON \
-    -DPython_ROOT_DIR=${PREFIX} ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCOMP_GUI=ON -DCOMP_PLUGINS=ON \
+    -DCOMP_COREBINARIES=ON -DCOMP_RSTATS=ON \
+    -DCOMP_ATLASES=ON -DCOMP_SUMA=ON \
+    -DCOMP_TCSH=ON -DCOMP_PYTHON=ON \
+    -DSTANDARD_PYTHON_INSTALL=ON \
+    -DPython_ROOT_DIR=${PREFIX} \
+    -DDOWNLOAD_TEST_DATA=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+
 make -j4 V=1
 make install
+
+# activate/deactivate scripts
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cp ${RECIPE_DIR}/scripts/activate.sh ${PREFIX}/etc/conda/activate.d/${PKG_NAME}.sh
+cp ${RECIPE_DIR}/scripts/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}.sh

3f03380fb1752d9b5f1832521c29a1c7323b8af5
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-08-22 18:17:46 -0500

    Set path variable

--- recipes/cnvcaller/20240118/build.sh
+++ recipes/cnvcaller/20240118/build.sh
@@ -11,3 +11,13 @@ cp *.py ${PREFIX}/bin/
 chmod +x ./bin/*.py
 chmod +x ./bin/*.pl
 cp ./bin/* ${PREFIX}/bin/
+
+# set CNVCALLER_ROOT variable on env activation
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/cnvcaller.sh
+export CNVCALLER_ROOT=${PREFIX}
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/cnvcaller.sh
+unset CNVCALLER_ROOT
+EOF

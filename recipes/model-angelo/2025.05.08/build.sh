8dc738fa49edf1408604966060e5f2468de83e48
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2025-12-05 16:33:02 -0600

    Add recipe for model-angelo

---
+++ recipes/model-angelo/2025.05.08/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+$PYTHON -m pip install --no-deps --ignore-installed --no-cache-dir -vvv .
+
+# create folder for models download
+target=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}
+mkdir -p ${target}/db/
+
+# copy script to download models
+chmod +x ${RECIPE_DIR}/download-db.sh
+cp ${RECIPE_DIR}/download-db.sh ${PREFIX}/bin
+
+# set TORCH_HOME variable on env activation
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/modelangelo.sh
+export TORCH_HOME=${target}/db/
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/modelangelo.sh
+unset TORCH_HOME
+EOF
